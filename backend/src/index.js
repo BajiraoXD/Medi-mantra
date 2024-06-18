3aadca1090f85135ea32cd8da498c829eb6365fe
Nilay <91113915+BajiraoXD@users.noreply.github.com>
2024-06-19 00:00:15 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -92,7 +92,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); 
+app.set('views', path.join(__dirname,'frontend', 'views')); 
 app.use('/api', router);
 
 
